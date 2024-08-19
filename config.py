1cf0527566c7ed696b07a47469f884c0654903f5
surajislam <154444093+surajislam@users.noreply.github.com>
2024-08-19 16:56:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(getenv("OWNER_ID", 6903379369))
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/surajislam/ANIE-2")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/surajislam/ANIE-3")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
